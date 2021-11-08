5b900195beead200d1b07161973332d1787134f4
YD <80422401@qq.com>
2021-11-08 13:59:59 +0800

    axios 请求防抖

--- src/axios/config.js
+++ src/axios/config.js
@@ -6,41 +6,68 @@ import {
   Spin,
   Message
 } from 'view-design';
-// axios.defaults.baseURL = ''
 
-// if (process.env.NODE_ENV == 'development') {
-//   //本地(开发)环境
-//   axios.defaults.baseURL = '/api';
-// } else if (process.env.NODE_ENV == 'production') {
-//   //线上（开发）环境
-//   axios.defaults.baseURL = '';
-// }
+let reqList = []
+
+/**
+ * 阻止重复请求
+ * @param {array} reqList - 请求缓存列表
+ * @param {string} url - 当前请求地址
+ * @param {function} cancel - 请求中断函数
+ * @param {string} errorMessage - 请求中断时需要显示的错误信息
+ */
+ const stopRepeatRequest = function (reqList, url, cancel, errorMessage) {
+  const errorMsg = errorMessage || ''
+  for (let i = 0; i < reqList.length; i++) {
+    if (reqList[i] === url) {
+      cancel(errorMsg)
+      return
+    }
+  }
+  reqList.push(url)
+}
+
+/**
+ * 允许某个请求可以继续进行
+ * @param {array} reqList 全部请求列表
+ * @param {string} url 请求地址
+ */
+const allowRequest = function (reqList, url) {
+  for (let i = 0; i < reqList.length; i++) {
+    if (reqList[i] === url) {
+      reqList.splice(i, 1)
+      break
+    }
+  }
+}
+
+if (process.env.NODE_ENV == 'development') {
+  //本地(开发)环境
+  // axios.defaults.baseURL = '/api';
+  axios.defaults.baseURL = 'http://192.168.188.28:3000';
+} else if (process.env.NODE_ENV == 'production') {
+  //线上（开发）环境
+  axios.defaults.baseURL = '';
+}
 
 axios.defaults.timeout = 10000;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 // 请求拦截器
 axios.interceptors.request.use(
   config => {
-      const userid = sessionStorage.userid;
-      userid && (config.headers.userid = userid);
-      Spin.show({
-        render: h => {
-          return h('div', [
-            h('Icon', {
-              'style': 'animation: ani-demo-spin 1s linear infinite;color:#fff',
-              props: {
-                type: 'ios-loading',
-                size: 30
-              }
-            }),
-            h('div', {
-              'style': 'color:#fff'
-            }, '加载中...')
-          ]);
-        }
-      });
-      return config;
-
+    let cancel
+      // 设置cancelToken对象
+    config.cancelToken = new axios.CancelToken(function(c) {
+        cancel = c
+    })
+    if(config.url!='/api/Common/get_poster' && config.url !='/api/Address/getarea'){
+      // 阻止重复请求。当上个请求未完成时，相同的请求不会进行
+      stopRepeatRequest(reqList, config.url, cancel, `${config.url} 请求被中断`);
+    }
+      // const userid = sessionStorage.userid;
+      // userid && (config.headers.userid = userid);
+      Spin.show();
+    return config;
   },
   error => {
     return Promise.error(error);
@@ -49,6 +76,9 @@ axios.interceptors.request.use(
 // 响应拦截器
 axios.interceptors.response.use(
   response => {
+    setTimeout(() => {
+      allowRequest(reqList, response.config.url)
+    }, 1000);
     if (response.status === 200) {
       Spin.hide();
       return Promise.resolve(response);
@@ -57,6 +87,14 @@ axios.interceptors.response.use(
     }
   },
   error => {
+    if (axios.isCancel(error)) {
+      console.log(error.message);
+    } else {
+      // 增加延迟，相同请求不得在短时间内重复发送
+      setTimeout(() => {
+        allowRequest(reqList, error.config.url)
+      }, 1000)
+    }
     if (error.response.status) {
       switch (error.response.status) {
         // 401: 未登录
